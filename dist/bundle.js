d182a9a65fa710a36644db720722357586f7326e
Shamblonaut <shaheembn@gmail.com>
2024-04-12 13:08:47 +0530

    Setup basic webpack project

---
+++ dist/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("Hello Nimbus!");
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsNkIiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9uaW1idXMvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coXCJIZWxsbyBOaW1idXMhXCIpOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    
+    module: {
+        rules: [
+            {
+                test: /\.(html)$/,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "index.html",
+        }),
+    ],
+
+    mode: "development",
+    devtool: "inline-source-map",
+    devServer: {
+        static: "./dist",
+    }
+};
